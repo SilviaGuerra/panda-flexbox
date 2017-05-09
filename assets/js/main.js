c62efb92506f4b872978153368e6de40c0e40941
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-05-09 16:34:22 -0500

    restaura fotos sin flex

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var foto = document.getElementsByClassName("foto");
+var cerrar = document.getElementsByClassName("cerrar");
+
+for(i= 0; i < foto.length; i++){
+  foto[i].addEventListener("click", borrar);
+}
+
+function borrar(){
+  this.style.display = "none";
+}
+
+var regresar = document.getElementById("restaurar");
+regresar.addEventListener("click", restaurar);
+
+function restaurar(){
+  for(i = 0; i<foto.length; i++){
+    foto[i].style.display = "flex"
+  }
+}
